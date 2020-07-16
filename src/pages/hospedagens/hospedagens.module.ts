e2d2602394c6870e7e3a9c631e48281a6e5fae0f
Osvando Neto <osvaldosneto@hotmail.com>
2020-07-16 13:16:31 -0300

    desativando menu tela login

---
+++ src/pages/hospedagens/hospedagens.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HospedagensPage } from './hospedagens';
+
+@NgModule({
+  declarations: [
+    HospedagensPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HospedagensPage),
+  ],
+})
+export class HospedagensPageModule {}
