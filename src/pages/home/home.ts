965249fb287571981ab70c5f491ca4065aa51167
Osvando Neto <osvaldosneto@hotmail.com>
2020-07-16 12:19:04 -0300

    lazy Load

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +11,4 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
 }
